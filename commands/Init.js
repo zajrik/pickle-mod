00841bae3da84f5beab13b2e1708ace0200a79c9
Zack Campbell <zajrik@gmail.com>
2016-10-11 21:54:22 -0500

    Push modbot progress

--- commands/Init.js
+++ commands/Init.js
@@ -19,6 +19,10 @@ exports.default = class Kick extends Command
 
 	action(message, args, mentions, original) // eslint-disable-line no-unused-vars
 	{
-		this.bot.mod.initGuild(message.guild);
+		message.delete();
+		this.bot.mod.initGuild(message.guild).then(() =>
+		{
+			message.author.sendMessage(`I'm done setting up! Everything is ready for you to start using moderation commands on your server. I've attempted to create a role called 'Mod' which you can assign to users that you want to be allowed to use moderation commands. If you already had a role of that name then you can use that role to determine who can use moderation commands.`);
+		});
 	}
 };
