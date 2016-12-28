a34eae39ca1fa4daf6eaa7e3db8b4e1f18138851
Zack Campbell <zajrik@gmail.com>
2016-12-28 10:25:28 -0600

    Add member join/leave logging functionality

--- src/lib/ModBot.ts
+++ src/lib/ModBot.ts
@@ -1,5 +1,6 @@
 'use strict';
 import { Bot, BotOptions } from 'yamdbf';
+import { GuildMember, TextChannel, RichEmbed, Message } from 'discord.js';
 import ModActions from './ModActions';
 import TimerCollection from './timer/TimerCollection';
 import Timer from './timer/Timer';
@@ -13,5 +14,25 @@ export default class ModBot extends Bot
 	{
 		super(botOptions);
 		this.timers = new TimerCollection<string, Timer>();
+
+		this.on('guildMemberAdd', (member: GuildMember) => this.memberLog(member, true, 8450847));
+		this.on('guildMemberRemove', (member: GuildMember) => this.memberLog(member, false, 13091073));
+	}
+
+	/**
+	 * Handle creation and sending of an embed for logging guild member joins/leaves.
+	 * Requires a text channel called `member-log` to be created that the bot is allowed
+	 * to post to. Won't do anything if the channel does not exist
+	 */
+	private memberLog(member: GuildMember, joined: boolean, color: number): Promise<Message>
+	{
+		if (!member.guild.channels.exists('name', 'member-log')) return;
+		const memberLog: TextChannel = <TextChannel> member.guild.channels.find('name', 'member-log');
+		const embed: RichEmbed = new RichEmbed()
+			.setColor(color)
+			.setAuthor(`${member.user.username}#${member.user.discriminator} (${member.id})`, member.user.avatarURL)
+			.setFooter(joined ? 'User joined' : 'User left' , '')
+			.setTimestamp();
+		return memberLog.sendEmbed(embed);
 	}
 }
