4cb9b233ae3dfbbab7c99a673fc39a3f603eeed2
Zack Campbell <zajrik@gmail.com>
2017-01-02 03:55:02 -0600

    Update Info command

--- src/commands/Info.ts
+++ src/commands/Info.ts
@@ -1,6 +1,6 @@
-import { Bot, Command, version } from 'yamdbf';
+import { Bot, Command, version, Message } from 'yamdbf';
+import { User, RichEmbed, Guild } from 'discord.js';
 import * as Discord from 'discord.js';
-import { User, Message, RichEmbed } from 'discord.js';
 import Time from '../lib/Time';
 
 export default class Info extends Command
@@ -19,16 +19,17 @@ export default class Info extends Command
 
 	public action(message: Message, args: Array<string | number>, mentions: User[], original: string): any
 	{
-		message.delete();
+		const prefix: string = message.guild ? message.guild.storage.getSetting('prefix') : '';
 		const embed: RichEmbed = new RichEmbed()
-			.setAuthor('YAMDBF Mod Info', this.bot.user.avatarURL)
 			.setColor(11854048)
+			.setAuthor('YAMDBF Mod Info', this.bot.user.avatarURL)
 			.addField('Mem Usage', `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
 			.addField('Uptime', Time.difference(this.bot.uptime * 2, this.bot.uptime).toString(), true)
 			.addField('\u200b', '\u200b', true)
 			.addField('Servers', this.bot.guilds.size.toString(), true)
 			.addField('Channels', this.bot.channels.size.toString(), true)
-			.addField('Users', this.bot.guilds.map(g => g.memberCount).reduce((a, b) => a + b), true)
+			.addField('Users', this.bot.guilds.map((g: Guild) =>
+				g.memberCount).reduce((a: number, b: number) => a + b), true)
 			.addField('YAMDBF', `v${version}`, true)
 			.addField('Discord.js', `v${Discord.version}`, true)
 			.addField('\u200b', '\u200b', true)
@@ -36,7 +37,8 @@ export default class Info extends Command
 			.addField('YAMDBF Info', 'https://yamdbf.js.org', true)
 			.addField('Bot Invite', `[Click here](https://discordapp.com/oauth2/authorize`
 				+ `?permissions=490826759&scope=bot&client_id=${this.bot.user.id})`, true)
-			.addField('\u200b', 'Be sure to use the `guide` command for information on setting up your server for moderation!')
+			.addField('\u200b', `Be sure to use the \`${prefix}guide\` command for information `
+				+ `on setting up your server for moderation!`)
 			.setFooter('YAMDBF', this.bot.user.avatarURL)
 			.setTimestamp();
 
