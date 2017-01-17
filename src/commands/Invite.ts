a04ef18f354a2037eeab980ceaf9abb027638c38
Zack Campbell <zajrik@gmail.com>
2017-01-16 20:10:57 -0600

    Update invite command output text
    Forgot to before, oops

--- src/commands/Invite.ts
+++ src/commands/Invite.ts
@@ -18,6 +18,12 @@ export default class Invite extends Command<Bot>
 
 	public action(message: Message, args: Array<string | number>, mentions: User[], original: string): void
 	{
-		message.channel.send(`You can invite me to your server with this link:\nhttps://discordapp.com/oauth2/authorize?client_id=${this.bot.user.id}&scope=bot&permissions=297888791\n\nAfter adding me to your server, I will send you a DM with instructions to prepare your server for moderation. Thanks for choosing YAMDBF Mod for your server moderation control needs! 👏`);
+		message.channel.send(`You can invite me to your server with this link:\n`
+			+ `https://discordapp.com/oauth2/authorize?client_id=${this.bot.user.id}&scope=bot&permissions=297888791\n\n`
+			+ `Be sure to use the \`guide\` command for information `
+			+ `on setting up your server for moderation! The default prefix for commands is \`?\`. `
+			+ `You can change this with the \`setprefix\` command.\n\nIf you ever forget the command prefix, `
+			+ `just use \`@${this.bot.user.username}#${this.bot.user.discriminator} prefix\`. `
+			+ `Thanks for choosing YAMDBF Mod for your server moderation control needs! 👏`);
 	}
 }
