d2d3bc13a451fcdfb339cc1a6b212fa841699ab4
Zack Campbell <zajrik@gmail.com>
2017-01-23 01:09:02 -0600

    Add sourcemaps to build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var source = require('gulp-sourcemaps');
 var del = require('del');
 
 gulp.task('default', () =>
 {
 	del.sync(['./bin/**/*.*']);
 	gulp.src('./src/**/*.ts')
+		.pipe(source.init())
 		.pipe(ts({
 			noImplicitAny: true,
 			outDir: 'bin',
 			target: 'ES6',
 			module: 'commonjs',
-			moduleResolution: 'node'
+			moduleResolution: 'node',
+			sourceMap: true
 		}))
+		.pipe(source.write('../bin/', { sourceRoot: '../src' }))
 		.pipe(gulp.dest('bin/'));
 	gulp.src('./src/config.json')
 		.pipe(gulp.dest('bin/'));
