a5b46e048f2133eaaec038ef22440ed47902626c
Zack Campbell <zajrik@gmail.com>
2017-08-25 04:12:10 -0500

    Update build tooling, deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,52 @@
 const gulp = require('gulp');
 const gulp_ts = require('gulp-typescript');
-const gulp_tslint = require('gulp-tslint');
 const gulp_sourcemaps = require('gulp-sourcemaps');
-const tslint = require('tslint');
 const del = require('del');
 const path = require('path');
+const { execSync } = require('child_process');
 
 const project = gulp_ts.createProject('tsconfig.json');
-const linter = tslint.Linter.createProgram('tsconfig.json');
+
+let _linter;
+let _gulp_tslint;
+let _tslint;
+let _runSequence;
+
+const runSequence = () => _runSequence = _runSequence || require('run-sequence');
+const gulp_tslint = () => _gulp_tslint = _gulp_tslint || require('gulp-tslint');
+const tslint = () => _tslint = _tslint || require('tslint');
+const linter = () => _linter = _linter || tslint().Linter.createProgram('tsconfig.json');
 
 gulp.task('default', ['build']);
+gulp.task('build:vscode', cb => runSequence()('lint', 'build', cb));
+
+gulp.task('pause', cb => setTimeout(() => cb(), 1e3));
 
 gulp.task('lint', () => {
-	gulp.src('./src/**/*.ts')
-		.pipe(gulp_tslint({
+	gulp.src('src/**/*.ts')
+		.pipe(gulp_tslint()({
 			configuration: 'tslint.json',
 			formatter: 'prose',
-			program: linter
+			program: linter()
 		}))
-		.pipe(gulp_tslint.report());
+		.pipe(gulp_tslint().report());
 })
 
 gulp.task('build', () => {
-	del.sync(['./bin/**/*.*']);
-	const tsCompile = gulp.src('./src/**/*.ts')
-		.pipe(gulp_sourcemaps.init())
+	del.sync(['bin/**/*.*']);
+	const tsCompile = gulp.src('src/**/*.ts')
+		.pipe(gulp_sourcemaps.init({ base: 'src' }))
 		.pipe(project());
 
 	tsCompile.pipe(gulp.dest('bin/'));
 
-	gulp.src('./src/**/*.js')
-		.pipe(gulp.dest('bin/'));
-
-	gulp.src('./src/**/*.json')
-		.pipe(gulp.dest('bin/'));
+	gulp.src('src/**/*.js').pipe(gulp.dest('bin/'));
+	gulp.src('src/**/*.json').pipe(gulp.dest('bin/'));
+	gulp.src('src/**/*.lang').pipe(gulp.dest('bin/'));
 
 	return tsCompile.js
-		.pipe(gulp_sourcemaps.write({
-			sourceRoot: file => path.relative(path.join(file.cwd, file.path), file.base)
-		}))
+		.pipe(gulp_sourcemaps.mapSources(sourcePath => path.join(__dirname, 'src', sourcePath)))
+		.pipe(gulp_sourcemaps.write())
 		.pipe(gulp.dest('bin/'));
 });
+	
